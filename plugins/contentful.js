474d6132940dd017aa2982bd7a53feb9bea12329
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2020-06-10 14:17:31 +0900

    contentful.jsの修正

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,10 +1,16 @@
 const contentful = require('contentful')
 
+
+const config = {
+  CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+  CTF_ACCESS_TOKEN: process.env.CTF_ACCESS_TOKEN
+}
+
 module.exports = {
-  createClient() {
+  createClient(config) {
     return contentful.createClient({
-      space: process.env.CTF_SPACE,
-      accessToken: process.env.CTF_ACCESS_TOKEN
+      space: config.CTF_SPACE,
+      accessToken: config.CTF_ACCESS_TOKEN
     })
   }
 }
