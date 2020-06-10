b9c099db1293c2f746b533c26ca6db7793eaf115
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2020-06-10 13:59:45 +0900

    修正

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,7 +1,10 @@
-import {
-  createClient
-} from 'contentful'
-export default createClient({
-  space: process.env.CTF_SPACE_ID,
-  accessToken: process.env.CTF_ACCESS_TOKEN
-})
+const contentful = require('contentful')
+
+module.exports = {
+  createClient() {
+    return contentful.createClient({
+      space: process.env.CTF_SPACE,
+      accessToken: process.env.CTF_ACCESS_TOKEN
+    })
+  }
+}
